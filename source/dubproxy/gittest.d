752afbb7bf9f0631f23618612adb5e6a23cafb3e
Robert Schadek <rschadek@symmetryinvestments.com>
2019-08-14 10:52:59 +0100

    and another test passes

--- source/dubproxy/gittest.d
+++ source/dubproxy/gittest.d
@@ -11,4 +11,7 @@ unittest {
 	TagReturn[] tags = getTags(dpf.getPath("dubproxy"));
 	string h = getHashFromVersion(tags, "v0.0.1");
 	assert(h == "4b9d4852c219e9eb062481f39d67118cfdd66664", h);
+
+	h = getHashFromVersion(tags, "v0.0.2");
+	assert(h == "78623dea2c9706c5622372c69e68df3b4779fb39", h);
 }
